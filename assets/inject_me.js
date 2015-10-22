e62dbffefc8e4236c621acfcab48391446f86128
emmajane <emma@emmajane.net>
2015-10-22 22:47:35 +0100

    SVGInjector: install library explicitly; wait for reveal before loading
    - Wait for reveal to be ready before triggering injector library
    - Use SVGInjector library directly instead of through a npm-installed package.

--- assets/inject_me.js
+++ assets/inject_me.js
@@ -1,5 +1,7 @@
+Reveal.addEventListener("ready",function() {
 // Select SVG files based on class name
 var mySVGsToInject = document.querySelectorAll('img.inject_me');
 
 // Replace the IMG with SVG objects so external style sheets will load
 SVGInjector(mySVGsToInject);
+});
